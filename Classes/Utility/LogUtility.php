9ff23422513ca2a1221d206acde52f9f54c82663
Oliver Kleinecke <okleinecke@web.de>
2018-06-26 08:35:07 +0200

    Update LogUtility - Remove deprecated TYPO3_DB call
    Oops .. did not see this last occurence of TYPO3_DB usage before .. fixed now too ;)

--- Classes/Utility/LogUtility.php
+++ Classes/Utility/LogUtility.php
@@ -72,22 +72,24 @@ class LogUtility {
      * @return integer
      */
     protected static function writeToSysLog($message, $userId = 0, $type = 255, $action = 0, $error = 0) {
-        $fields_values = array(
-            'userid' => (int) $userId,
-            'type' => (int) $type,
-            'action' => (int) $action,
-            'error' => (int) $error,
-            'details_nr' => 1,
-            'details' => $message,
-            'log_data' => serialize(array()),
-            'tablename' => '',
-            'IP' => (string) GeneralUtility::getIndpEnv('REMOTE_ADDR'),
-            'tstamp' => $GLOBALS['EXEC_TIME'],
-            'event_pid' => -1,
-            'workspace' => '-99'
+        /** @var \TYPO3\CMS\Extbase\Object\ObjectManager $objectManager */
+		$objectManager = GeneralUtility::makeInstance(ObjectManager::class);
+		/** @var \TYPO3\CMS\Core\Database\Query\QueryBuilder $queryBuilder */
+		$queryBuilder = $objectManager->get(ConnectionPool::class)->getQueryBuilderForTable('be_users');
+		return $queryBuilder->insert('sys_log')
+					->values(array(
+						'userid' => (int) $userId,
+						'type' => (int) $type,
+						'action' => (int) $action,
+						'error' => (int) $error,
+						'details_nr' => 1,
+						'details' => $message,
+						'log_data' => serialize(array()),
+						'tablename' => '',
+						'IP' => (string) GeneralUtility::getIndpEnv('REMOTE_ADDR'),
+						'tstamp' => $GLOBALS['EXEC_TIME'],
+						'event_pid' => -1,
+						'workspace' => '-99'
+					))->execute();
         );
-        $GLOBALS['TYPO3_DB']->exec_INSERTquery('sys_log', $fields_values);
-        return $GLOBALS['TYPO3_DB']->sql_insert_id();
-    }
-
 }
