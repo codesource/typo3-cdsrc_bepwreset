e9838a63149e08989204fc8329f905ef70bea3f7
ochorocho <rothjochen@gmail.com>
2018-10-05 10:18:17 +0200

    Raise TYPO3 version to 9.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'Code-Source',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-8.7.99',
+            'typo3' => '8.7.0-9.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
